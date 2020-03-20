b7a9087eef5bdc6cd0867c777555527a92487d23
langdonglei <langdonglei@sina.com>
2020-03-20 23:07:46 +0800

    m

--- route/route.php
+++ route/route.php
@@ -6,22 +6,23 @@ Route::miss(function () {
     ], 404);
 });
 Route::post('/github_webhook', function () {
-    $s1 = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-    $s2 = 'sha1=' . hash_hmac('sha1', file_get_contents('php://input'), Env::get('GITHUB_WEBHOOK_SECRET'));
-    if ($s1 == $s2) {
-        $path = Env::get('root_path');
-        $proc = proc_open("cd $path && git pull", [1 => ['pipe', 'w'], 2 => ['pipe', 'w']], $pipes);
-        echo stream_get_contents($pipes[1]);
-        echo stream_get_contents($pipes[2]);
-        fclose($pipes[1]);
-        fclose($pipes[2]);
-        proc_close($proc);
-
-    }
-    return json([
-        'code'    => 1,
-        'message' => 'ok'
-    ], 200);
+//    $s1 = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+//    $s2 = 'sha1=' . hash_hmac('sha1', file_get_contents('php://input'), Env::get('GITHUB_WEBHOOK_SECRET'));
+//    if ($s1 == $s2) {
+//        $path = Env::get('root_path');
+//        $proc = proc_open("cd $path && git pull", [1 => ['pipe', 'w'], 2 => ['pipe', 'w']], $pipes);
+//        echo stream_get_contents($pipes[1]);
+//        echo stream_get_contents($pipes[2]);
+//        fclose($pipes[1]);
+//        fclose($pipes[2]);
+//        proc_close($proc);
+//
+//    }
+//    return json([
+//        'code'    => 1,
+//        'message' => 'ok'
+//    ], 200);
+    return 123444555666;
 });
 Route::get('banner/one', 'api/BannerController/one');
 Route::get('category/all', 'api/CategoryController/all');
