f07bb05964a6269dc962570bba34edcde6cd13b5
langdonglei <langdonglei@sina.com>
2020-03-20 22:33:40 +0800

    m

--- route/route.php
+++ route/route.php
@@ -5,12 +5,11 @@ Route::miss(function () {
         'message' => 'url not found'
     ], 404);
 });
-Route::get('/hook', function () {
-    return 334455;
+Route::get('/github_webhook', function () {
     $s1 = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-    $s2 = 'sha1=' . hash_hmac('sha1', file_get_contents('php://input'), $_ENV['GITHUB_WEBHOOK_SECRET']);
+    $s2 = 'sha1=' . hash_hmac('sha1', file_get_contents('php://input'), Env::get('GITHUB_WEBHOOK_SECRET'));
     if ($s1 == $s2) {
-        $path = base_path();
+        $path = Env::get('root_path');
         $proc = proc_open("cd $path && git pull", [1 => ['pipe', 'w'], 2 => ['pipe', 'w']], $pipes);
         echo stream_get_contents($pipes[1]);
         echo stream_get_contents($pipes[2]);
